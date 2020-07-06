a222ee3bc5b22fa13b9e8343e19a76f034146341
Björn Åhström <ahstrom_81@hotmail.com>
2020-07-06 17:41:45 +0200

    Initial Commit

---
+++ BinaryWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BinaryWatch
+//
+//  Created by Björn Åhström on 2020-07-06.
+//  Copyright © 2020 Björn Åhström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BinaryWatchTests/BinaryWatchTests.swift
@@ -0,0 +1,34 @@
+//
+//  BinaryWatchTests.swift
+//  BinaryWatchTests
+//
+//  Created by Björn Åhström on 2020-07-06.
+//  Copyright © 2020 Björn Åhström. All rights reserved.
+//
+
+import XCTest
+@testable import BinaryWatch
+
+class BinaryWatchTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
